8a92fd588773ac0a82309ca6e4fae3330ec9e807
jinuine505 <nguyenj0708@gmail.com>
2025-06-24 23:19:01 -0400

    Tweak padding

--- tailwind-practice/src/components/Hero.jsx
+++ tailwind-practice/src/components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
         <div>
             {/* Hero columns */}
-            <div className="w-full flex flex-col md:flex-row pt-30 pb-5 mx-auto">
+            <div className="w-full flex flex-col md:flex-row pt-30 pb-2 mx-auto">
                 {/* Left column */}
                 <div className="w-full flex items-center text-5xl sm:text-7xl lg:text-8xl xl:text-9xl font-bold bg-orange">
                     <span className="flex p-5 text-cream items-center">
@@ -22,6 +22,7 @@ const Hero = () => {
                     All Watches
                 </a>
             </div>
+            {/* Men's and women's buttons */}
             <div className="container flex mx-auto pb-10 gap-2 px-5">
                 <a className={`relative w-full flex justify-center bg-cream nav-link-bg-green duration-200 text-lg sm:text-2xl p-1 z-0 font-medium hover:text-cream`} href="#products">
                     Men's
